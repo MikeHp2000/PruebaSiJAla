c746fb2647a73286a5918131c81099b601d397d6
MikeHp2000 <121138598+MikeHp2000@users.noreply.github.com>
2023-02-14 09:56:57 -0600

    Add files via upload

---
+++ Boceto/Boceto/MenuViewController.swift
@@ -0,0 +1,32 @@
+//
+//  MenuViewController.swift
+//  Boceto
+//
+//  Created by iOSLabMini01 on 13/02/23.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func Volvermenu (segue: UIStoryboardSegue ) {
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

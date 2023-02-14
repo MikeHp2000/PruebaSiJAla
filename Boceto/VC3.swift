d0194433cb343b64643a1ba36f54c223ea2244d7
MikeHp2000 <121138598+MikeHp2000@users.noreply.github.com>
2023-02-14 10:17:10 -0600

    Add files via upload

---
+++ Boceto/VC3.swift
@@ -0,0 +1,30 @@
+//
+//  VC3.swift
+//  Boceto
+//
+//  Created by iOSLabMini01 on 13/02/23.
+//
+
+import UIKit
+
+class VC3: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
